1f3973fc11102760f3f4098cd3121e46ed49ce7e
Jaebin Lim <113482924+JaebinLimm@users.noreply.github.com>
2025-08-13 15:53:10 +0900

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('connect')
@@ -24,5 +23,6 @@ def index():
 if __name__ == '__main__':
     print("WebSocket server is running (http://<server IP>:5000)")
     socketio.run(app, host='0.0.0.0', port=5000)
+
 
 
